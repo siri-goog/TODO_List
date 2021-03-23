2f6cf68bd99c47e49d403a58d7787d838419726d
Siripond <goog.mullanu@gmail.com>
2021-03-23 12:11:52 +1100

    Update login layout and styles

--- routes/signup.js
+++ routes/signup.js
@@ -5,7 +5,10 @@ const db = require('../database')
 const crypto = require('crypto');
 
 router.get('/', (req, res) => {
-    res.render('pages/signup')
+    res.render('pages/signup', {
+        username: req.session.username,
+        user_id: req.session.user_id
+    })
 })
 
 module.exports = router
