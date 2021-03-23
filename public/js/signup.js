c6e8aac4f4eb48ae48e53def576a3270e3dac0e1
Siripond <goog.mullanu@gmail.com>
2021-03-24 01:17:11 +1100

    Update signup validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,10 +1,16 @@
 //Function: check required field
 function checkRequired(input, alert) {
+    
     var result = true
 
     if (input.value === "") {
-        console.log("Please enter " + input.id + ".")
-        alert.innerHTML = "Please enter " + input.id + "."
+        //console.log("Please enter " + input.id + ".")
+        if(input.id == "pswRepeat"){
+            alert.innerHTML = "Please enter repeat password" 
+        }else{
+            console.log(input.id)
+            alert.innerHTML = "Please enter " + input.id + "."
+        }
         input.focus()
         result = false;
     } else {
@@ -16,18 +22,19 @@ function checkRequired(input, alert) {
 
 //Function: validate email
 function validateEmail(email, emailAlert) {
+    
     var result = true
+    
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRegex.test(email.value.toLowerCase())) {
 
-    var atPosition = email.value.indexOf("@");
-    var dotPosition = email.value.lastIndexOf(".");
-
-    if (atPosition < 1 || ( dotPosition - atPosition < 2 )) {
+        emailAlert.innerHTML = ""
+        
+    }else {
         console.log("Please enter a valid e-mail address.")
         emailAlert.innerHTML = "Please enter a valid e-mail address."
         email.focus()
         result = false;
-    } else {
-        emailAlert.innerHTML = ""
     }
 
     return result
@@ -36,65 +43,65 @@ function validateEmail(email, emailAlert) {
 //Function: validate password 
 function validatePassword(password, passwordAlert) {
     var result = true
+    
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if (passwordRegex.test(password.value)) {
 
-    var strongRegex = new RegExp("^(?=.{14,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
-
-    if (strongRegex.test(password.value)) {
-        console.log("Please enter a valid e-mail address.")
-        passwordAlert.innerHTML = "Please enter a valid e-mail address."
+        passwordAlert.innerHTML = ""
+        
+    }else {
+        console.log(password.value)
+        passwordAlert.innerHTML = "Please enter a valid password."
         password.focus()
         result = false;
-    }else {
-        passwordAlert.innerHTML = ""
     }
 
     return result
 }
 
 function validate() {
+    
     var finalResult = true
 
-    var firstname = document.getElementById("firstname")
-    var lastname = document.getElementById("lastname")
+    var userName = document.getElementById("username")
     var email = document.getElementById("email")
     var password = document.getElementById("password")
-    var confirmPassword = document.getElementById("confirmPassword")
+    var pswRepeat = document.getElementById("pswRepeat")
 
-    var alertFName = document.getElementById("alertFirstname")
-    var alertLName = document.getElementById("alertLastname")
-    var alertEmail = document.getElementById("alertEmail")
-    var alertPassword = document.getElementById("alertPassword")
-    var alertConfirmPassword = document.getElementById("alertConfirmPassword")
+    var usernameMsg = document.getElementById("usernameMsg")
+    var pswMsg = document.getElementById("pswMsg")
+    var pswRepeatMsg = document.getElementById("pswRepeatMsg")
+    var emailMsg = document.getElementById("emailMsg")
 
     //check required fields
-    if (checkRequired(firstname, alertFName) === false) {
+    
+    if (checkRequired(userName, usernameMsg) === false) {
+       
         finalResult = false
     }
-    if (checkRequired(lastname, alertLName) === false) {
+     if (checkRequired(email, emailMsg) === false) {
+        finalResult = false
+
+    }  else if (validateEmail(email, emailMsg) === false) {
+        finalResult = false
+    }
+    if (checkRequired(password, pswMsg) === false) {
+        finalResult = false
+    } else if (validatePassword(password, pswMsg) === false) {
+        finalResult = false
+    }
+    if (checkRequired(pswRepeat, pswRepeatMsg) === false) {
         finalResult = false
     } 
-    if (checkRequired(email, alertEmail) === false) {
+    if (password.value !== pswRepeat.value) {
+        pswRepeatMsg.innerHTML = "Password doesn't match"
+        pswRepeat.focus()
         finalResult = false
-    } else if (validateEmail(email, alertEmail) === false) {
-        finalResult = false
-    }
-    if (checkRequired(password, alertPassword) === false) {
-        finalResult = false
-    } else if (validatePassword(confirmPassword, alertConfirmPassword) === false) {
-        finalResult = false
-    }
-    if (checkRequired(confirmPassword, alertConfirmPassword) === false) {
-        finalResult = false
-    } else if (password.value !== confirmPassword.value) {
-        alertConfirmPassword.innerHTML = "Password doesn't match"
-        confirmPassword.focus()
-
-        finalResult = false
-    }
+    }   
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const registerBtn = document.getElementById("registerBtn")
+    const registerBtn = document.getElementById("signupBtn")
     registerBtn.onclick = (event) => {
         validate()
     }
