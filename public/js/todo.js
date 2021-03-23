92612cf8ee4a28d25108adcc871797545f6ec8b7
Siripond <goog.mullanu@gmail.com>
2021-03-24 01:18:05 +1100

    Update todo function

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,44 +1,73 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+//Get to-do list from database
+$(document).ready(function(){
+  let link = '/todo/loadTodo'
+  $.getJSON(link)
+  .then(data => {
+    if(data.data !== "") {
+      console.log(data)
+      for (let i=0; i<data.data.length; i++) {
+        var listTodo = $('body').find('#listTodo')
+        var itemTodo = $('<li>')
+        itemTodo.prop('id', data.data[i].todo_id)
+        itemTodo.text(data.data[i].description)
+        listTodo.append(itemTodo)
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+        var iconClose = $('<span>')
+        iconClose.prop('class', "close")
+        iconClose.text("\u00D7")
+        itemTodo.append(iconClose)
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+        /*var li = document.createElement("li")
+        var inputValue = data.data[i].description
+        var title = document.createTextNode(inputValue)
+        li.appendChild(title)
+        document.getElementById("listTodo").appendChild(li)
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
+        var span = document.createElement("SPAN")
+        var txt = document.createTextNode("\u00D7")
+        span.className = "close"
+        span.appendChild(txt)
+        li.appendChild(span)*/
+      }
+
+      for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var item = this.parentElement;
+          //item.style.display = "none";
+          console.log(item)
+          fetch('/todo/deleteTodo/'+item.id, {method: 'POST'})
+          .then(function(response) {
+            if(response.ok) {
+              console.log('Item was deleted');
+              return;
+            }
+            throw new Error('Request failed');
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+        }
+      }
+    } 
+  })
+  .catch(err => {
+    console.log(err)
+  });
+})
+
+// Add new to-do (Create a new list item when clicking on the "Add" button)
+/*function addTodo() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+
+  var inputValue = document.getElementById("title").value;
+  var title = document.createTextNode(inputValue);
+  li.appendChild(title);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("listTodo").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("title").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -52,4 +81,34 @@ function newElement() {
       div.style.display = "none";
     }
   }
+  
+}*/
+
+// Close button (Create a "close" button and append it to each list item)
+var todolist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < todolist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  todolist[i].appendChild(span);
 }
+
+// Delete item (Click on a close button to hide the current list item)
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Check Completed item (Add a "checked" symbol when clicking on a list item)
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
--- routes/todo.js
+++ routes/todo.js
@@ -9,5 +9,54 @@ router.get('/', (req, res) => {
         username: req.session.username
     })
 })
-
+//--Get to-do details
+router.get('/loadTodo', (req, res) => {
+    db.any('SELECT * FROM todo WHERE user_id = $1;', [req.session.user_id])
+    .then((data) => {
+        if (data.length > 0) {
+            res.json({
+                data: data
+            })
+        } else {
+            res.json({
+                data: ""
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.render('pages/error')
+    })
+})
+//--Add to-do
+router.post('/addTodo', (req, res) => {
+    const { desc } = req.body
+    const status = 1 //In progress
+    try {
+        db.query('INSERT INTO todo (user_id, description, status) VALUES ($1, $2, $3);', [req.session.user_id, desc, status])
+        .then (() => {
+            res.redirect('/todo')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+//--Delete to-do
+router.post('/deleteTodo/:todo_id', (req, res) => {
+    const todo_id = req.params.todo_id
+    try {
+        db.query('DELETE FROM todo WHERE todo_id = $1;', [todo_id])
+        .then (() => {
+            res.redirect('/todo')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 module.exports = router
