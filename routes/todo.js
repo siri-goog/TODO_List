fb6df6f42b322739db0b77564fec7a8aef6f8e7c
Siripond <goog.mullanu@gmail.com>
2021-03-23 17:59:12 +1100

    Update todo function

--- routes/todo.js
+++ routes/todo.js
@@ -4,12 +4,9 @@ const validator = require('validator');
 const db = require('../database')
 const crypto = require('crypto');
 
-let username = ""
-
 router.get('/', (req, res) => {
     res.render('pages/todo', {
-        username: username,
-        user_id: req.session.user_id
+        username: req.session.username
     })
 })
 
